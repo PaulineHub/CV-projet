b91cc3bb0ed3a1e45a58714ed29427e464ef3422
Pauline <huby.pauline@gmail.com>
2021-12-13 17:35:44 -0500

    page realisation responsive et modif footer

--- menu.js
+++ menu.js
@@ -1,6 +1,9 @@
 const menuIcon = document.getElementById("menu-icon");
 const nav = document.getElementById("nav");
 
+const education = document.getElementById("education");
+const height = document.getElementById("height-grow");
+
 menuIcon.addEventListener("click", function(){
     nav.classList.toggle("show-menu");
 });
