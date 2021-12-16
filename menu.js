abedc8d694c224b889bedf7ac110eb0bfaec286e
Pauline <huby.pauline@gmail.com>
2021-12-16 14:06:00 -0500

    projet final

--- menu.js
+++ menu.js
@@ -1,9 +1,7 @@
 const menuIcon = document.getElementById("menu-icon");
 const nav = document.getElementById("nav");
 
-const education = document.getElementById("education");
-const height = document.getElementById("height-grow");
-
 menuIcon.addEventListener("click", function(){
     nav.classList.toggle("show-menu");
 });
+
